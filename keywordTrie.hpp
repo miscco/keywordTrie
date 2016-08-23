4265e76b5c3582acdd156b9cb53352d2e9ddc722
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-08-23 12:45:32 +0200

    Added a remark about Bernhards original code

--- keywordTrie.hpp
+++ keywordTrie.hpp
@@ -1,6 +1,9 @@
 /*
 * Copyright (C) 2016 Michael Schellenberger Costa.
 *
+* This code is based on a C-implementation of the keyword trie construction,
+* preprocessing and text search by Bernhard Haubold.
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
